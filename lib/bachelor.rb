a478df2927dfab53998190fefd43c899b3d8c31f
Daniel Schneider <daschne8@gmail.com>
2019-03-05 20:19:10 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -19,7 +19,15 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  hometown_count = 0
+  data.each do |season,season_array|
+    season_array.each do |contestant|
+      if contestant["hometown"] == hometown
+        hometown_count++
+      end
+    end
+  end
+  hometown_count
 end
 
 def get_occupation(data, hometown)
